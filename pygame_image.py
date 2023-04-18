938ab83dc5074532a3900fb5b573ca9a737b6c58
korenaga <c0b211814f@edu.teu.ac.jp>
2023-04-18 16:00:00 +0900

    練習問題6

--- pygame_image.py
+++ pygame_image.py
@@ -18,8 +18,9 @@ def main():
             if event.type == pg.QUIT: return
 
         tmr += 1
-        screen.blit(bg_img, [0, 0])
-        screen.blit(kk_img, [0, 0])
+        screen.blit(bg_img, [-tmr, 0])
+        screen.blit(bg_img, [1600-tmr, 0])
+        screen.blit(kk_imgs[tmr%2], [300, 200])
 
         pg.display.update()
         clock.tick(100)
